926841080edb32a993104fb308f98a1179695508
Jamon <daniel.ospinaperez@gmail.com>
2018-12-09 05:59:07 -0500

    added Node entity

---
+++ src/main/java/com/metro/subterraneo/model/Node.java
@@ -0,0 +1,53 @@
+package com.metro.subterraneo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@IdClass(Node.class)
+@Table(name = "node")
+public class Node implements Serializable {
+
+	@Id
+	@Column(name = "route")
+	@NotEmpty(message = "Please provide a route")
+	private Route route;
+	
+	@Id
+	@Column(name = "station")
+	@NotEmpty(message = "Please provide a station")
+	private Station station;
+	
+	public Node (
+			@NotEmpty(message = "Please provide a route") Route route,
+			@NotEmpty(message = "Please provide a station") Station station
+			) {
+		super();
+		this.station = station;
+		this.route = route;
+	}
+	
+	public Node () {
+		super();
+	}
+
+	public Route getRoute() {
+		return route;
+	}
+
+	public void setRoute(Route route) {
+		this.route = route;
+	}
+
+	public Station getStation() {
+		return station;
+	}
+
+	public void setStation(Station station) {
+		this.station = station;
+	}
+
+	
+}
