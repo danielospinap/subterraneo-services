1c76d3774b7c2e9f66c4f7f2302e4b7739c76617
Jamon <daniel.ospinaperez@gmail.com>
2018-12-10 21:46:59 -0500

    endpoint for get shortest paths

--- src/main/java/com/metro/subterraneo/controller/GraphRestController.java
+++ src/main/java/com/metro/subterraneo/controller/GraphRestController.java
@@ -1,17 +1,43 @@
 package com.metro.subterraneo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jgrapht.GraphPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.metro.subterraneo.entity.PathResponse;
+import com.metro.subterraneo.model.Edge;
+import com.metro.subterraneo.model.Node;
+import com.metro.subterraneo.model.Route;
+import com.metro.subterraneo.service.PathService;
 
 @RestController
 @RequestMapping
 public class GraphRestController {
+	
+	@Autowired
+	private PathService pathService;
 
-	@GetMapping(value= "/greet")
-	public ResponseEntity<String> greet() {
-		return new ResponseEntity<>("hello world", HttpStatus.OK);
+	@GetMapping(value= "/{from}/{to}/{paths}")
+	public ResponseEntity<List<PathResponse>> paths(@PathVariable String from, @PathVariable String to, @PathVariable int paths) {
+		List<GraphPath<Node,DefaultWeightedEdge>> graphPaths = pathService.findShortestPath(from, to, paths);
+		
+		List<PathResponse> pathsForResponse = new ArrayList<PathResponse>();
+		for (int i = 0; i < graphPaths.size(); i++) {
+			List<Node> path = graphPaths.get(i).getVertexList();
+			double weight = graphPaths.get(i).getWeight() - 2;
+			
+			pathsForResponse.add(new PathResponse(path, weight));
+		}
+		
+		return new ResponseEntity<List<PathResponse>>(pathsForResponse, HttpStatus.OK);
 	}
 }
