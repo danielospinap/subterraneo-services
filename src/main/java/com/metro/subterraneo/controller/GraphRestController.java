27940a29f824e0df4256dde709bb94aac432a2c0
Jamon <daniel.ospinaperez@gmail.com>
2018-12-17 00:10:57 -0500

    added cors support in GraphRestController

--- src/main/java/com/metro/subterraneo/controller/GraphRestController.java
+++ src/main/java/com/metro/subterraneo/controller/GraphRestController.java
@@ -8,6 +8,7 @@ import org.jgrapht.graph.DefaultWeightedEdge;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import com.metro.subterraneo.model.Node;
 import com.metro.subterraneo.model.Route;
 import com.metro.subterraneo.service.PathService;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping
 public class GraphRestController {
